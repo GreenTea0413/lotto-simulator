fd29197ffb607a85404791905b146765e27421f5
GreenTea0413 <kimbosung22@naver.com>
2025-10-25 00:24:44 +0900

    [feat] head.tsx 추가

---
+++ data/luckySpots.ts
@@ -0,0 +1,16 @@
+// /data/luckySpots.ts
+export const luckySpots = [
+  {
+    name: "노량진 로또명당",
+    address: "서울 동작구 노량진로 123",
+    lat: 37.5145,
+    lng: 126.9423,
+  },
+  {
+    name: "종로 복권방",
+    address: "서울 종로구 종로 100",
+    lat: 37.5702,
+    lng: 126.9830,
+  },
+  // ...더 추가 가능
+];

2cfd011e5ede44be68878e9d54629da5893dbb9c
GreenTea0413 <kimbosung22@naver.com>
2025-10-29 23:43:23 +0900

    [feat] 인라인 <script> 허용

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' https://dapi.kakao.com 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src * blob: data:;
+              connect-src *;
+              font-src 'self' https://fonts.gstatic.com;
+              frame-src *;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
